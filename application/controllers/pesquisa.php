81ec1e58a8d5f0333d2c75a9f906767bb89a1d60
Vitor Atair <vitor.atair@intelbras.com.br>
2013-07-16 22:27:50 -0300

    - Fixbug paginação modelo / marca & Adicionado paginação pesqquisa por palavra

--- application/controllers/pesquisa.php
+++ application/controllers/pesquisa.php
@@ -28,82 +28,152 @@ class Pesquisa extends CI_Controller {
 	 */
 	public function listAll()
 	{
-
 		// Verifica se existe dados de formulário //
-		$ncm 	= $this->input->post('ncm');
-		$ano 	= $this->input->post('ano');	
-		$marca 	= $this->input->post('marca');
+		$ncm 			= $this->input->post('ncm');
+		$year 			= $this->input->post('ano');	
+		$brand 			= $this->input->post('marca');
+		$model 			= $this->input->post('modelo');
+		$search 		= $this->input->post('search');
+		$unsearch 		= $this->input->post('unsearch');
+		$control 		= $this->input->post('controle');
 
-		// Carrega a view correspondende //
-		$data['main_content'] = 'pesquisa/pesquisa_view';
-		
 		// Carrega os dados necessários da model //
 		$data['ncms'] 	= $this->ncm_model->listar();
 		$data['anos'] 	= $this->ncm_model->listarAno();					
-		$data['marcas'] = $this->marca_model->listarAllMarca();			
+		$data['marcas'] = $this->marca_model->listarAllMarca();	
 
-		// Caso o usuário não tenha escolhido ncm e ano, recebe os dados da sessão //
-		if (empty($ncm) && (empty($ano)))
+		// Caso a variável ncm esteja vazia, recebe os dados que estão em sessão //
+		if (empty($ncm))
 		{
-			$ncm = $this->session->userdata('ncm');
-			$ano = $this->session->userdata('ano');				
+			$ncm 	= $this->session->userdata['ncm'];
+			$year 	= $this->session->userdata['year'];
+		}
+		else
+		{
+			$session['ncm'] = $ncm;
+			$session['year'] = $year;
+			$this->session->set_userdata($session);
 		}
 
-		// Montando o nome da tabela
-		$table = $ncm . "_" . $ano;
-
-		// verifica se a variável table só possui "_" //
-		if (strlen($table) > 3)
+		if (empty($brand) && (empty($control)))
 		{
+			$brand = $this->session->userdata['brand'];
+		}
+		else
+		{
+			$session['brand'] = $brand;
+			$this->session->set_userdata($session);	
+		}		
+		if (empty($model) && (empty($control)))
+		{
+			$model = $this->session->userdata['model'];
+		}
+		else
+		{
+			$session['model'] = $model;
+			$this->session->set_userdata($session);	
+		}
+		if (empty($search) && (empty($control)))
+		{
+			$search = $this->session->userdata['search'];
+		}
+		else
+		{
+			$session['search'] = $search;
+			$this->session->set_userdata($session);	
+		}			
 
-			// Salvando ncm e ano em sessão //
-			$data['ncm'] = $ncm;
-			$data['ano'] = $ano;
-			$this->session->set_userdata($data);
+		// Montando a nome da tabela a ser procurada //
+		$table 	= $ncm . "_" . $year;
 
-			// Configurando paginação //
+		// Verfica se a tabela possui 13 caracteres, um valor menor que 13 significa que a tabela não possui ou a ncm ou o ano //
+		if (strlen($table) == 13)
+		{
+			
+	        // Configurando a url base para paginação //
 	        $config["base_url"] 	= base_url() . "index.php/pesquisa/listAll";
+			
+			// Carrega o array com a ncm e o ano para a view //
+			$data['ncm'] 	= $ncm;
+			$data['year'] 	= $year;
 
-			if (!empty($marca) && (empty($modelo)))
+			if (!empty($search))
 			{
-				// Carrega todos os modelos da marca selecionada //
-				$data['modelos'] = $this->modelo_model->buscaModeloByMarca($marca);			
 
-		        $config["total_rows"] 	= $this->ncm_model->countBuscaDados($table,'2', $marca);
+				// Configurando paginação //
+		        $config["total_rows"] 	= $this->ncm_model->countBuscaDados($table,'4',NULL, NULL, $search);
 		        $config["per_page"] 	= 20;
 		        
 		        $this->pagination->initialize($config);
 		        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
 
 				// Carrega os dados somente com ano e ncm //
-				$data['dados'] = $this->ncm_model->buscaDados($config['per_page'], $page, $table, '2', $marca);
-				$data["links"] = $this->pagination->create_links();				
-			}
-			elseif (empty($marca) && (empty($modelo)))
-			{
+				$data['dados'] 	= $this->ncm_model->buscaDados($config['per_page'], $page, $table, '4', NULL, NULL, $search);
+				$data["links"] 	= $this->pagination->create_links();
 
-		        $config["total_rows"] 	= $this->ncm_model->countBuscaDados($table,'1',NULL);
+			}
+			elseif (empty($brand))
+			{
+				
+				// Configurando paginação //
+		        $config["total_rows"] 	= $this->ncm_model->countBuscaDados($table,'1',NULL, NULL, NULL);
 		        $config["per_page"] 	= 20;
 		        
 		        $this->pagination->initialize($config);
 		        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
 
 				// Carrega os dados somente com ano e ncm //
-				$data['dados'] = $this->ncm_model->buscaDados($config['per_page'], $page, $table, '1', NULL);
-				$data["links"] = $this->pagination->create_links();			
-			}
+				$data['dados'] 	= $this->ncm_model->buscaDados($config['per_page'], $page, $table, '1', NULL, NULL, NULL);
+				$data["links"] 	= $this->pagination->create_links();				
 
+			}
+			else
+			{				
+				if (empty($model))
+				{
+					// Carrega todos os modelos da marca selecionada //
+					$data['modelos'] = $this->modelo_model->buscaModeloByMarca($brand);			
+
+			        $config["total_rows"] 	= $this->ncm_model->countBuscaDados($table,'2', $brand, NULL, NULL);
+			        $config["per_page"] 	= 20;
+			        
+			        $this->pagination->initialize($config);
+			        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+					// Carrega os dados somente com ano e ncm //
+					$data['dados'] = $this->ncm_model->buscaDados($config['per_page'], $page, $table, '2', $brand, NULL, NULL);
+					$data["links"] = $this->pagination->create_links();									
+				}
+				else
+				{
+					// Carrega todos os modelos da marca selecionada //
+					$data['modelos'] = $this->modelo_model->buscaModeloByMarca($brand);			
+
+			        $config["total_rows"] 	= $this->ncm_model->countBuscaDados($table,'3', $brand, $model, NULL);			        			      
+			        $config["per_page"] 	= 20;
+			        
+			        $this->pagination->initialize($config);
+			        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+					// Carrega os dados somente com ano e ncm //
+					$data['dados'] = $this->ncm_model->buscaDados($config['per_page'], $page, $table, '3', $brand, $model, NULL);
+					$data["links"] = $this->pagination->create_links();	
+				}
+			}
+		}
+		else
+		{
+			// Exibe somente uma tela em branco para o usuário
 
 		}
+
+		// Carrega a view correspondende //
+		$data['main_content'] = 'pesquisa/pesquisa_view';
 
 		// Envia todas as informações para tela //
 		$this->parser->parse('template', $data);
 
 	}
-
-
-
-
 }
 
 ?>
