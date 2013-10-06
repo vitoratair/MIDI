80a590187c50b3c1ec7fd0278634fa53bb92bc4e
Vitor Atair <vitor.ruts@gmail.com>
2013-10-06 10:04:59 -0300

    Adicionado no HTML pesquisa por mês.

--- application/views/analyze/analise_view_empty.php
+++ application/views/analyze/analise_view_empty.php
@@ -14,15 +14,15 @@
 <div class="container"> 
 
     <!--  SELEÇÃO DE CATEGORIA -->
-    <table width="100%" class="table table-bordered">
-        <tr>
-            <td>                
-                <?php
-                    $atributos = array('form class'=>' form-horizontal',  'id'=>'FormCadastro', 'method'=>'POST');
-                    echo form_open('analyze/listAll', $atributos); 
-                ?>
+<?php
+    $atributos = array('form class'=>' form-horizontal',  'id'=>'FormCadastro', 'method'=>'POST');
+    echo form_open('analyze/listAll', $atributos); 
+?>    
+        <table width="100%" class="">
+            <tr>
+                <td>                
                     <div align="left">
-                        <select id="categoria"  name="categoria" class="span3" onchange="this.form.submit()">                   
+                        <select id="categoria"  name="categoria" class="span3">                   
                             
                             <option value=""> Selecione uma categoria </option>                 
                             {categorias}    
@@ -30,10 +30,57 @@
                             {/categorias}
                             
                         </select>
+                    </div>  
+                </td>
+
+                <!-- Form para data da pesquisa -->
+                <td>
+                    <div align="right">
+                        <select id="dataInicial"  name="dataInicial" class="span3">                   
+                            
+                            <option value="1"> Data inicial </option>                 
+
+                                <option value="1">Janeiro</option>
+                                <option value="2">Fevereiro</option>
+                                <option value="3">Março</option>
+                                <option value="4">Abril</option>
+                                <option value="5">Maio</option>
+                                <option value="6">Junho</option>
+                                <option value="7">Julho</option>
+                                <option value="8">Agosto</option>
+                                <option value="9">Setembro</option>
+                                <option value="10">Outubro</option>
+                                <option value="11">Novembro</option>
+                                <option value="12">Dezembro</option>
+                            
+                        </select>
                     </div>
-                </form>
-            </td>
-        </tr>
-    </table><!--  FIM DA SELEÇÃO DE CATEGORIA -->
-   
+                </td>
+
+                <td>
+                    <div align="left">
+                        <select id="dataFinal"  name="dataFinal" class="span3" onchange="this.form.submit()">                   
+                            
+                            <option value="1"> Data Final </option>                 
+
+                                <option value="12">Janeiro</option>
+                                <option value="2">Fevereiro</option>
+                                <option value="3">Março</option>
+                                <option value="4">Abril</option>
+                                <option value="5">Maio</option>
+                                <option value="6">Junho</option>
+                                <option value="7">Julho</option>
+                                <option value="8">Agosto</option>
+                                <option value="9">Setembro</option>
+                                <option value="10">Outubro</option>
+                                <option value="11">Novembro</option>
+                                <option value="12">Dezembro</option>
+                            
+                        </select>
+                    </div>
+                </td>
+            </tr>
+        </table><!--  FIM DA SELEÇÃO DE CATEGORIA -->
+    </form>
+
 </div>
--- application/views/analyze/analyzeSubcategory_view.php
+++ application/views/analyze/analyzeSubcategory_view.php
@@ -14,26 +14,81 @@
 <div class="container"> 
 
     <!--  MENU -->
-
-    <table width="100%" class="table table-bordered">
+<?php
+    $atributos = array('form class'=>' form-horizontal',  'id'=>'FormCadastro', 'method'=>'POST');
+    echo form_open('analyze/listAll', $atributos); 
+?>
+    <table width="100%" class="" border="0px">
         <tr>
             <td>                
-                <?php
-                    $atributos = array('form class'=>' form-horizontal',  'id'=>'FormCadastro', 'method'=>'POST');
-                    echo form_open('analyze/listAll', $atributos); 
-                ?>
-                    <div align="left">
-                        <select id="categoria"  name="categoria" class="span3" onchange="this.form.submit()">                   
-                            
-                            <option value=""> Selecione uma categoria </option>                 
-                            {categorias}    
-                                <option value="{CID}">{CNome}</option>
-                            {/categorias}
-                            
-                        </select>
-                    </div>
-                </form>
+                <div align="left">
+                    <select id="categoria"  name="categoria" class="span3">                   
+                        
+                        <option value=""> Selecione uma categoria </option>                 
+                        {categorias}    
+                            <option value="{CID}">{CNome}</option>
+                        {/categorias}
+                        
+                    </select>
+                </div>
+                
+            </td>
+            <!-- Form para data da pesquisa -->
+            <td>
+                <div align="right">
+                    <select id="dataInicial"  name="dataInicial" class="span3">                   
+                        
+                        <option value="1"> Data inicial </option>                 
 
+                            <option value="1">Janeiro</option>
+                            <option value="2">Fevereiro</option>
+                            <option value="3">Março</option>
+                            <option value="4">Abril</option>
+                            <option value="5">Maio</option>
+                            <option value="6">Junho</option>
+                            <option value="7">Julho</option>
+                            <option value="8">Agosto</option>
+                            <option value="9">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        
+                    </select>
+                </div>
+            </td>
+
+            <td>
+                <div align="left">
+                    <select id="dataFinal"  name="dataFinal" class="span3" onchange="this.form.submit()">                   
+                        
+                        <option value="1"> Data Final </option>                 
+
+                            <option value="12">Janeiro</option>
+                            <option value="2">Fevereiro</option>
+                            <option value="3">Março</option>
+                            <option value="4">Abril</option>
+                            <option value="5">Maio</option>
+                            <option value="6">Junho</option>
+                            <option value="7">Julho</option>
+                            <option value="8">Agosto</option>
+                            <option value="9">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        
+                    </select>
+                </div>
+            </td>
+        </tr>
+</form>
+        <tr>
+            <td colspan="3">
+                <hr>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="3">
                 <?php
                     $atributos = array('form class'=>'form-horizontal',  'id'=>'FormCadastro', 'method'=>'POST');
                     echo form_open('analyze/listAll', $atributos); 
