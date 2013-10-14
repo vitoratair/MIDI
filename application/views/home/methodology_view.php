c6b872c9178e9e05f286da934f26de37bb6f6a14
Vitor Atair <vitor.ruts@gmail.com>
2013-10-13 21:15:43 -0300

    Adicionado links para as páginas de documentação, metodologia e nova pesquisa
       - Links estão apontando para as páginas corretas porém o conteúdo
         ainda não foi criado.

---
+++ application/views/home/methodology_view.php
@@ -0,0 +1,10 @@
+<br>
+<!--=== Content Part ===-->
+<div class="container"> 
+    
+    <p align="center">
+        <img width="35%" src="<?php echo base_url();?>assets/img/construcao.png">    
+    </p>
+
+</div>
+
