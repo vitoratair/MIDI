68460d9e30a598c9f02a8e21a2dd58ffa1da4cb5
Vitor Atair <vitor.ruts@gmail.com>
2013-12-05 11:25:23 -0200

    Fixbux: filtros na página busca de importações

--- application/views/home/home_view.php
+++ application/views/home/home_view.php
@@ -1,73 +1,91 @@
+
 <br><br>
 
-
-<!--=== Purchase Block ===-->
-<div class="row-fluid purchase margin-bottom-30">
-    <div class="container">
-        <div class="span12">
+<body onload="reload()">
     
-    <p align="center">
-        <h1 align="center">Consulte análises realizadas</h1>
-    </p>
-    <p align="center">            
-        <a href="<?php echo base_url();?>index.php/analyze/listAll" class="btn-buy hover-effect"> Inicie aqui </a>
-        </p>
-        </div>
+    <div class="row-fluid purchase margin-bottom-30">
+        <div class="container">
+            <div class="span12">
         
+        <p align="center">
+            <h1 align="center">Consulte análises realizadas</h1>
+        </p>
+        <p align="center">            
+            <a href="<?php echo base_url();?>index.php/analyze/listAll" class="btn-buy hover-effect"> Inicie aqui </a>
+            </p>
+            </div>
+            
+        </div>
     </div>
-</div><!--/row-fluid-->
-<br><br>
 
-<!--=== Content Part ===-->
-<div class="container"> 
-    <!-- Service Blocks -->    
-    <div class="row-fluid">
-        <a href="<?php echo base_url();?>index.php/app/methodology">
-        <div class="span4">
-            <div class="service clearfix">
-                <i class="icon-cogs"></i>
-                <div class="desc">
-                    <h4>Metodologia</h4>
-                    <p>
-                        Detalhamento de como são obtidos e processados os dados
-                    </p>
-                </div>
-            </div>  
-        </div>
-        </a>
+    <br><br>
 
-        <a href="<?php echo base_url();?>index.php/app/newSearch">
-        <div class="span4">
-            <div class="service clearfix">
-                <i class="icon-question-sign"></i>
-                <div class="desc">
-                    <h4>Nova pesquisa</h4>
-                    <p>
-                      O que é necessário para adicionar uma nova pesquisa? 
-                    </p>
-                </div>
-            </div>  
-        </div>
-        </a>
+    <!--=== Content Part ===-->
+    <div class="container"> 
+        <!-- Service Blocks -->    
+        <div class="row-fluid">
+            <a href="<?php echo base_url();?>index.php/app/methodology">
+            <div class="span4">
+                <div class="service clearfix">
+                    <i class="icon-cogs"></i>
+                    <div class="desc">
+                        <h4>Método</h4>
+                        <p>
+                            Como os dados são obtidos e processados   
+                            <!-- Detalhamento de como são obtidos e processados os dados -->
+                        </p>
+                    </div>
+                </div>  
+            </div>
+            </a>
 
-        <a href="<?php echo base_url();?>index.php/app/documentation">
-        <div class="span4">
-            <div class="service clearfix">
-                <i class="icon-book"></i>
-                <div class="desc">
-                    <h4>Documentação</h4>
-                    <p>                        
-                        Manual de utilização do sistema, principais funções passo-a-passo                        
-                    </p>
-                </div>
-            </div>  
-        </div>              
-        </a>
-    </div><!--/row-fluid-->
+            <a href="<?php echo base_url();?>index.php/app/newSearch">
+            <div class="span4">
+                <div class="service clearfix">
+                    <i class="icon-question-sign"></i>
+                    <div class="desc">
+                        <h4>Nova pesquisa</h4>
+                        <p>
+                          O que é necessário para adicionar uma nova pesquisa? 
+                        </p>
+                    </div>
+                </div>  
+            </div>
+            </a>
 
+            <a href="<?php echo base_url();?>index.php/app/documentation">
+            <div class="span4">
+                <div class="service clearfix">
+                    <i class="icon-book"></i>
+                    <div class="desc">
+                        <h4>Documentação</h4>
+                        <p>                        
+                            Principais funções passo-a-passo
+                        </p>
+                    </div>
+                </div>  
+            </div>              
+            </a>
+        </div> <!-- End Purchase Block -->
+    </div>
 
-<!-- End Purchase Block -->
+</body>
 
-    <!-- //End Service Blokcs -->
-</div>
+<script type="text/javascript">
+    
+    function reload()
+    {
+       var url = window.location.href;
+       
+       if (url.indexOf("?") > -1 )
+       {
+            newUrl = url.substr(0,url.indexOf("?"));
+       }
+       
+       if (url != newUrl)
+       {
+            window.location = newUrl;
+       }
+    }
 
+</script>
