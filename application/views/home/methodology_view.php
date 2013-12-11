d64aede785069e3628dfbabb3d44d3e7a62dace2
Vitor Atair <vitor.ruts@gmail.com>
2013-12-10 23:27:34 -0200

    Iniciado o desenvolvimento da página sobre metodologia de pesquisa utilizada no MIDI

--- application/views/home/methodology_view.php
+++ application/views/home/methodology_view.php
@@ -1,65 +1,202 @@
-<!--=== Breadcrumbs ===-->
-<div class="row-fluid breadcrumbs margin-bottom-40">
-	<div class="container">
-        <h1 class="pull-left">Metodologia <small>de processamento</small></h1>
-        <ul class="pull-right breadcrumb">
-            <li><a href="<?php echo base_url();?>index.php/app/home">Home</a> <span class="divider"> / </span></li>
-            <li><a href="<?php echo base_url();?>index.php/model/listAll">Cadastro</a> <span class="divider"> / </span></li>
-            <li class="active">Adicionar itens</li>
-        </ul>
-    </div><!--/container-->
-</div><!--/breadcrumbs-->
 
-<!--=== Content Part ===-->
-<div class="container portfolio-item"> 	
-	<div class="row-fluid margin-bottom-20"> 
-		<!-- Carousel -->
-        <div class="span7">
-            <div id="myCarousel" class="carousel slide">
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img src="<?php echo base_url();?>assets/img/carousel/5.jpg" alt="">
-                        <div class="carousel-caption">
-                            <h4>First Thumbnail label</h4>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta.</p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="assets/img/carousel/4.jpg" alt="">
-                        <div class="carousel-caption">
-                            <h4>Second Thumbnail label</h4>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta.</p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="assets/img/carousel/3.jpg" alt="">
-                        <div class="carousel-caption">
-                            <h4>Third Thumbnail label</h4>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta.</p>
-                        </div>
-                    </div>
+<br><br><br><br><br><br><br>
+<div class="container-fluid one-page">
+
+    <div class="one-page-inner one-blue">
+        <div class="container">
+            <!-- <h1>Busca de dados</h1> -->
+            
+            <div class="row-fluid">
+                
+                <div class="span6">
+                    <img src="<?php echo base_url();?>assets/img/receita.png" class="margin-bottom-10" width="50%" alt="">
                 </div>
-                <div class="carousel-arrow">
-                    <a data-slide="prev" href="#myCarousel" class="left carousel-control"><i class="icon-angle-left"></i></a>
-                    <a data-slide="next" href="#myCarousel" class="right carousel-control"><i class="icon-angle-right"></i></a>
+
+                <div class="span6">
+                    <p><br><br><br>
+                        <h1>Mensalmente são disponibilizados pela <strong>Receita Federal</strong>
+                        dados dos produtos importados, agrupados por <strong>NCM</strong>.</h1>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    
+<!--     <div align="center">
+        <img src="<?php echo base_url();?>assets/img/seta1.png" class="margin-bottom-10" width="150px" alt="">
+    </div> -->
+    <br><br><br>
+    <div class="one-page-inner one-default">
+        <div class="container">
+            <div class="row-fluid margin-bottom-42">
+                <div class="span12">
+                    <p><h1>Entre os dados fornecidos estão:
+                        <strong>Descrição detalhada do produto</strong>
+                        país de origem, preço FOB e quantidade comercializada.
+                        </h1>
+                    </p>
                 </div>
             </div>
-        </div><!--/span7-->
-        <!-- //End Tabs and Carousel -->
+            <hr>
+            <div class="row-fluid margin-bottom-42">
+                <div class="span12">
+                    <div class=" ">
+                        <h1>A Receita não informa a <strong>marca</strong> e o <strong>modelo</strong>
+                            dos produtos. Estas informações devem ser buscadas no texto da <strong>descrição
+                            detalhada do produto</strong></h1>
+                    </div>
+                </div>
 
-        <div class="span5">
-        	<h3>MIDI - Monitoramento Intelbras de Dados de importações</h3>
-            <p>At vero eos et accusamus et iusto odio dignissimos <a href="#">ducimus qui blanditiis</a> praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum Fusce condimentum eleifend enim a feugiat. Pellentesque viverra vehicula sem ut volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non libero magna. Sed et quam lacus.</p>
-            <p>Molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Ut non libero consectetur adipiscing elit magna. Sed et quam lacus.</p>
-            <ul class="unstyled">
-            	<li><i class="icon-user color-green"></i> Jack Baur</li>
-            	<li><i class="icon-calendar color-green"></i> 14,2003 February</li>
-            	<li><i class="icon-tags color-green"></i> Websites, Google, HTML5/CSS3</li>
-            </ul>
-            <p><a class="btn-u btn-u-large" href="#">VISIT THE PROJECT</a></p>
+            </div>
         </div>
-    </div><!--/row-fluid-->
+    </div>
 
-               
-</div><!--/container-->	 	
-<!--=== End Content Part ===-->
+<!--     <div align="center">
+        <img src="<?php echo base_url();?>assets/img/seta3.png" class="margin-bottom-10" width="100px" alt="">
+    </div> -->
+
+    <br><br><br>
+    <div class="one-page-inner one-blue">
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span12">
+                    <p>
+                        <h1>
+                            Esta descrição pode ser completa, contendo <strong>marca</strong> e <strong>modelo</strong>...
+                        </h1>
+                        
+                        <div class=""><h2>
+                            REF.:TCF-2000 - APARELHOS TELEFONICOS, FIXO COM FIO, SEM FONTE
+                            PROPRIA DE ENERGIA, MONOCANAL, MARCA <strong><i>ELGIN</i></strong>,
+                            COD: HCD-81, MODELO <strong><i>TCF-2000</i></strong>,
+                            COR PRETO
+                        <h2>
+                        </div>  
+                        
+                    </p>    
+
+                    <p>
+                        <h1>
+                            Ou incompleta, permitindo muitas vezes apenas a identificação da categoria...
+                        </h1>
+                        
+                        <div class="alert alert-danger">
+                            <br><strong><i>TELEFONE COM FIO</i></strong> - REF: RM-9047<br><br>
+                        </div>                        
+                    </p> 
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="one-page-inner one-default">
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span6">
+                    <p>
+                        <h1>
+                            O trabalho do <strong>MIDI</strong> é buscar nestas descrições
+                            <strong>marcas</strong> e <strong>Modelos</strong>,
+                            possiblitando o agrupamento dos dados em
+                            <strong>categorias de produtos</strong>
+                        </h1>
+                    </p>
+
+                    <img src="<?php echo base_url();?>assets/img/seta1.png" class="margin-bottom-10" width="6%" align="center">
+                
+                    <div class="">  
+                        <h3>Telefone com fio, DVR, Switch, Terminal IP...</h3>
+                    </div>                    
+
+                </div>
+
+                <div class="span6">
+                    <img src="<?php echo base_url();?>assets/img/search.png" class="margin-bottom-10" width="70%" alt="">
+                </div>
+            </div>                    
+        </div>
+    </div> 
+
+    <div class="one-page-inner one-red">
+        <div class="container">
+            <div class="row-fluid margin-bottom-40">
+                <div class="span12">
+                    <p>
+                        <h2 align="center">
+                            Para que o <strong>MIDI</strong> realize estas buscas, é necessário
+                            alimentá-lo com bases de <strong>marcas</strong> e <strong>modelos</strong>.
+                            Você pode entender melhor como criá-las em<a class="btn-u btn-u-large btn-u-red one-page-btn"><i class="icon-pencil"></i> Nova pesquisa</a>
+                        
+                    </p>
+                </div>
+                 <div class="span12" align="center">
+                    <img src="<?php echo base_url();?>assets/img/Folhas.png" class="margin-bottom-10" width="50%">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="one-page-inner one-blue">
+        <div class="container">    
+            <div class="row-fluid">
+<!--                 <div class="span6">
+                    <img src="<?php echo base_url();?>assets/img/mao.png" class="margin-bottom-10" alt="">
+                </div> -->
+                <div class="span12">
+                    <p>
+                        <h2 align="center">
+                            Depois da análise automática, é necessário <strong>analisar</strong>
+                            manualmente as descrições que o <strong>MIDI</strong> não conseguiu
+                            processar por estarem <strong>incompletas</strong>.
+                        </h2>
+                    </p>
+
+                    <div class="span12">
+                        <div class="span2">
+                            <img src="<?php echo base_url();?>assets/img/seta3.png" class="" width="60px">
+                        </div>
+                        <div class="span10">
+                            <br><br><br><h2><strong>Refinamento dos dados</strong></h2>
+                        </div>                        
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <br><br><br>
+    <div class="one-page-inner one-default">
+        <div class="container">
+            <div class="row-fluid">
+
+                <div class="span5">
+                    <img src="<?php echo base_url();?>assets/img/grafico.png" class="margin-bottom-10" width="" >
+                </div>
+
+                <div class="span7">
+                    <p><h2 align="">
+                        Como resultado o <strong>MIDI</strong> entrega
+                        análises de importações por <strong>categoria</strong>,
+                        <strong>marca</strong> e <strong>modelo</strong>.<br><br>
+                        Gráficos e tabelas podem ser gerados com diferentes combinações
+                        de variáveis, auxiliando no processo de <strong>tomada de desições</strong>
+                    </h2></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+    <br><br><br>
+<!--         
+        <p align="center">
+            <img src="<?php echo base_url();?>assets/img/Logo_MIDI.jpg" class="margin-bottom-10" width="">
+        </p> -->
